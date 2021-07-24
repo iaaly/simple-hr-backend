5137535f64071bbbaa4deeecdba7242070c46d61
Abdelrahman <abdelrahmaniaaly@gmail.com>
2021-07-24 22:00:00 +0200

    Add key API for Github Laravel Action

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/key', function() {
+    return \Illuminate\Support\Str::random(32);
 });
 
 $router->group(['prefix' => 'departments'], function() use ($router) {
